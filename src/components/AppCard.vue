7b8ef0bcac8b73f5a2587ba31df75327f543eacb
SimoneBonvecchio84 <simonebonvecchio@gmail.com>
2024-05-06 17:51:54 +0200

    Add: bootstrap style rounded-circle to img

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -8,11 +8,17 @@ export default {
 
 <template>
    <div class="ms_card">
-    <img :src="cardObj.image" alt="CurImage">
+    <img
+     class="img-fluid rounded-circle"
+    :src="cardObj.image" alt="CurImage">
        <h4>{{ cardObj.name }}</h4>
+       <h6>{{ cardObj.status }}</h6>
+       <h6>{{ cardObj.species }}</h6>
    </div>
 </template>
 
 <style lang="scss" scoped>
-
+.ms_card {
+    text-align: center;
+}
 </style>
--- src/components/AppListCards.vue
+++ src/components/AppListCards.vue
@@ -18,9 +18,12 @@ export default {
 </script>
 
 <template>
+    <div class="text-center p-5">
+        <h2 class="fw-bolder">Rick and Morty App</h2>
+    </div>
    <div class="container">
-    <div class="row row-cols-1 row-cols-md-2 row-cols-lg-4">
-        <div class="col" v-for="card in cardsArray">
+    <div class="row row-cols-1 row-cols-md-2 row-cols-lg-4 gap-6">
+        <div class="p-2 col " v-for="card in cardsArray">
             <AppCard :cardObj="card"/>
         </div>
     </div>
@@ -28,4 +31,6 @@ export default {
    </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+
+</style>
